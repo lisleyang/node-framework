eae30288452c24e6c556500dc107ab7baf632fa4
lisleyang <lisleyang@163.com>
2017-11-17 18:02:38 +0800

    配置

--- config/webpack.conf.js
+++ config/webpack.conf.js
@@ -1,11 +1,78 @@
 const webpack = require('webpack');
 const fs = require("fs");
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const lodash = require("lodash");
 
 const pagesPath = path.join(__dirname, '../src/web/views');
 const widgetPath = path.join(__dirname, '../src/web/widget');
 // 处理所有的js的处理文件
 const jsEntries = {};
 fs.readdirSync(pagesPath).map((o, filename) => {
-    console.log(o);
+    const _fd = pagesPath + "/" + o;
+    fs.readdirSync(_fd).map((innero, infile) => {
+        //console.log(innero);
+        if (/\.entry\.js$/.test(innero)) {
+            jsEntries[innero.replace(".entry.js", "")] = _fd + "/" + o;
+        }
+    })
 })
+
+const _entries = Object.assign(jsEntries);
+const _module = {
+    rules: [{
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [{
+                loader: 'css-loader'
+            }, {
+                loader: 'postcss-loader',
+                options: {
+                    plugins: function() {
+
+                    }
+                }
+            }]
+        })
+    }, {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+            "preset": ["es2015", "stage-0"]
+        }
+    }]
+}
+
+const _resolve = {
+    extensions: [".js", ".css"]
+}
+const _devLoaders = _.clone(_module.rules);
+_devLoaders.push({
+    test: /\.(png|jpg|gif|eot|woff|woff2|tff|svg|otf)$/,
+    loader: "fileloader?name=assets/images/[name].[ext]"
+})
+const _prodLoaders = _.clone(_module.rules);
+_prodLoaders.push({
+    test: /\.(png|jpg|gif|eot|woff|woff2|tff|svg|otf)$/,
+    loader: "fileloader?name=assets/images/[name].[hash:5].[ext]"
+})
+
+const WebpackConfig = {
+    dev: {
+        entry: _entries,
+        module: {
+            rules: _devLoaders
+        },
+        resolve: _resolve
+    },
+    prod: {
+        entry: _entries,
+        module: {
+            rules: _prodLoaders
+        },
+        resolve: _resolve
+    }
+}
+
+export default WebpackConfig
---
+++ config/webpack.dev.js
@@ -0,0 +1,11 @@
+const conf = require("./wepack.conf");
+const options = {
+    output: {
+        path: path.join(__dirname, '../build/'),
+        publicPath: '/', //上线前用，所有css/js都要加到前面的
+        filename: "assets/scripts/[name].bundle.js"
+    }
+}
+const _options = Object.assign(conf.dev, options);
+
+module.exports = _options;
